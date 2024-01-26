48e755ef2171cfbe99065659dbb8058ec009ffea
Alexis <alexismengual@icloud.com>
2024-01-26 19:00:51 +0100

    Simple reformat

--- map.py
+++ map.py
@@ -4,6 +4,7 @@ import pyscroll
 
 from screen import Screen
 
+
 class Map:
     def __init__(self, screen):
         self.screen = screen
@@ -11,21 +12,19 @@ class Map:
         self.map_layer = None
         self.group = None
 
-        #swich to map0
+        # swich to map0
         self.switch_map("map0")
 
-
     def switch_map(self, map):
-        #load the wanted map
+        # load the wanted map
         self.tmx_data = pytmx.load_pygame(f"assets\\map\\{map}.tmx")
-        #get the map for pyscroll
+        # get the map for pyscroll
         map_data = pyscroll.data.TiledMapData(self.tmx_data)
-        #render the mapdata
+        # render the mapdata
         self.map_layer = pyscroll.BufferedRenderer(map_data, self.screen.get_size())
-        #puts everythong together
+        # puts everythong together
         self.group = pyscroll.PyscrollGroup(map_layer=self.map_layer, default_layer=7)
 
-
     def update(self):
-        #show the map on screen
+        # show the map on screen
         self.group.draw(self.screen.get_display())
