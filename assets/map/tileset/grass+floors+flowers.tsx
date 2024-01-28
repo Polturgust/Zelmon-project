5470388458380906d1bcb50354789d8f60aca719
Polturgust <florent.serr@gmail.com>
2024-01-28 22:04:32 +0100

    route0 finished + all objects set up

--- assets/map/tileset/grass+floors+flowers.tsx
+++ assets/map/tileset/grass+floors+flowers.tsx
@@ -1,4 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="grass+floors+flowers" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
  <image source="images/grass+floors+flowers.png" width="512" height="512"/>
+ <wangsets>
+  <wangset name="weeds" type="corner" tile="-1">
+   <wangcolor name="weeds" color="#ff0000" tile="-1" probability="1"/>
+  </wangset>
+ </wangsets>
 </tileset>
