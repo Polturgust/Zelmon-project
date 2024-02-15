e361d7e4c9642c71b1720f350ba29e03951b7c4d
Polturgust <florent.serr@gmail.com>
2024-02-15 22:34:08 +0100

    adding first interior map (can't spawn in it for some reason) + some tilesets

---
+++ sources/assets/map/tileset/interior_floors.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="interior_floors" tilewidth="16" tileheight="16" tilecount="1056" columns="16">
+ <image source="images/interior_floors.png" width="256" height="1056"/>
+</tileset>
