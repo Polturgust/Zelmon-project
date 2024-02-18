408143c17bba8ad0631aab29f361eb17ce864e87
Polturgust <florent.serr@gmail.com>
2024-02-18 13:30:09 +0100

    adding the proffessor's laboratory in ville0

--- sources/map.py
+++ sources/map.py
@@ -84,6 +84,9 @@ class Map:
                         elif j.name != None and "vers_interieur_mc_salon" in j.name:
                             dest = j.name.split("vers_interieur_mc_salon")
                             self.changes.add(Collisions(j.width, j.height, j.x, j.y, "interieur_mc_salon" + dest[1]))
+                        elif j.name != None and "vers_interieur_prof" in j.name:
+                            dest = j.name.split("vers_interieur_prof")
+                            self.changes.add(Collisions(j.width, j.height, j.x, j.y, "interieur_prof" + dest[1]))
 
                     # Si c'est un objet de la couche qui contient les points d'apparitions, on choisit le bon en fonction de la zone de laquelle le joueur arrive
                     if i.name == "points_de_spawn":
