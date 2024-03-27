271389f55f57978ed16ff09d6f8d12f804ba1322
Polturgust <florent.serr@gmail.com>
2024-03-27 20:28:07 +0100

    adding end map + creating MOAM png + adding missing pnj

---
+++ sources/assets/map/tileset/retour au jeu.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="retour au jeu" tilewidth="16" tileheight="16" tilecount="40" columns="20">
+ <image source="images/retour au jeu.png" width="320" height="32"/>
+</tileset>
