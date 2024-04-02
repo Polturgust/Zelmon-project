feb7e89d759e43134002d0794eae8f91e03afe94
Polturgust <florent.serr@gmail.com>
2024-04-02 09:50:01 +0200

    start of the master sword room

--- sources/assets/map/tileset/holly_water_for_animations.tsx
+++ sources/assets/map/tileset/holly_water_for_animations.tsx
@@ -1,6 +1,80 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="holly_water_for_animations" tilewidth="16" tileheight="16" tilecount="528" columns="66">
  <image source="images/holly_water_for_animations.png" width="1056" height="128"/>
+ <tile id="199">
+  <animation>
+   <frame tileid="199" duration="300"/>
+   <frame tileid="205" duration="300"/>
+   <frame tileid="211" duration="300"/>
+   <frame tileid="217" duration="300"/>
+   <frame tileid="223" duration="300"/>
+   <frame tileid="229" duration="300"/>
+   <frame tileid="235" duration="300"/>
+   <frame tileid="241" duration="300"/>
+   <frame tileid="247" duration="300"/>
+   <frame tileid="253" duration="300"/>
+   <frame tileid="259" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="200">
+  <animation>
+   <frame tileid="200" duration="300"/>
+   <frame tileid="206" duration="300"/>
+   <frame tileid="212" duration="300"/>
+   <frame tileid="224" duration="300"/>
+   <frame tileid="230" duration="300"/>
+   <frame tileid="236" duration="300"/>
+   <frame tileid="242" duration="300"/>
+   <frame tileid="248" duration="300"/>
+   <frame tileid="254" duration="300"/>
+   <frame tileid="260" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="201">
+  <animation>
+   <frame tileid="201" duration="300"/>
+   <frame tileid="207" duration="300"/>
+   <frame tileid="213" duration="300"/>
+   <frame tileid="219" duration="300"/>
+   <frame tileid="225" duration="300"/>
+   <frame tileid="231" duration="300"/>
+   <frame tileid="237" duration="300"/>
+   <frame tileid="243" duration="300"/>
+   <frame tileid="249" duration="300"/>
+   <frame tileid="255" duration="300"/>
+   <frame tileid="261" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="202">
+  <animation>
+   <frame tileid="202" duration="300"/>
+   <frame tileid="208" duration="300"/>
+   <frame tileid="214" duration="300"/>
+   <frame tileid="220" duration="300"/>
+   <frame tileid="226" duration="300"/>
+   <frame tileid="232" duration="300"/>
+   <frame tileid="238" duration="300"/>
+   <frame tileid="244" duration="300"/>
+   <frame tileid="250" duration="300"/>
+   <frame tileid="256" duration="300"/>
+   <frame tileid="262" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="265">
+  <animation>
+   <frame tileid="265" duration="300"/>
+   <frame tileid="271" duration="300"/>
+   <frame tileid="277" duration="300"/>
+   <frame tileid="283" duration="300"/>
+   <frame tileid="289" duration="300"/>
+   <frame tileid="295" duration="300"/>
+   <frame tileid="301" duration="300"/>
+   <frame tileid="307" duration="300"/>
+   <frame tileid="313" duration="300"/>
+   <frame tileid="319" duration="300"/>
+   <frame tileid="325" duration="300"/>
+  </animation>
+ </tile>
  <tile id="266">
   <animation>
    <frame tileid="266" duration="300"/>
@@ -29,6 +103,33 @@
    <frame tileid="315" duration="300"/>
    <frame tileid="321" duration="300"/>
    <frame tileid="327" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="268">
+  <animation>
+   <frame tileid="268" duration="300"/>
+   <frame tileid="274" duration="300"/>
+   <frame tileid="280" duration="300"/>
+   <frame tileid="286" duration="300"/>
+   <frame tileid="292" duration="300"/>
+   <frame tileid="298" duration="300"/>
+   <frame tileid="304" duration="300"/>
+   <frame tileid="310" duration="300"/>
+   <frame tileid="316" duration="300"/>
+   <frame tileid="322" duration="300"/>
+   <frame tileid="328" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="331">
+  <animation>
+   <frame tileid="331" duration="300"/>
+   <frame tileid="337" duration="300"/>
+   <frame tileid="343" duration="300"/>
+   <frame tileid="349" duration="300"/>
+   <frame tileid="355" duration="300"/>
+   <frame tileid="361" duration="300"/>
+   <frame tileid="367" duration="300"/>
+   <frame tileid="373" duration="300"/>
   </animation>
  </tile>
  <tile id="332">
@@ -61,6 +162,33 @@
    <frame tileid="393" duration="300"/>
   </animation>
  </tile>
+ <tile id="334">
+  <animation>
+   <frame tileid="334" duration="300"/>
+   <frame tileid="346" duration="300"/>
+   <frame tileid="352" duration="300"/>
+   <frame tileid="364" duration="300"/>
+   <frame tileid="370" duration="300"/>
+   <frame tileid="376" duration="300"/>
+   <frame tileid="382" duration="300"/>
+   <frame tileid="388" duration="300"/>
+   <frame tileid="394" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="397">
+  <animation>
+   <frame tileid="397" duration="300"/>
+   <frame tileid="403" duration="300"/>
+   <frame tileid="409" duration="300"/>
+   <frame tileid="415" duration="300"/>
+   <frame tileid="421" duration="300"/>
+   <frame tileid="433" duration="300"/>
+   <frame tileid="439" duration="300"/>
+   <frame tileid="445" duration="300"/>
+   <frame tileid="451" duration="300"/>
+   <frame tileid="457" duration="300"/>
+  </animation>
+ </tile>
  <tile id="398">
   <animation>
    <frame tileid="398" duration="300"/>
@@ -89,6 +217,80 @@
    <frame tileid="447" duration="300"/>
    <frame tileid="453" duration="300"/>
    <frame tileid="459" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="400">
+  <animation>
+   <frame tileid="400" duration="300"/>
+   <frame tileid="406" duration="300"/>
+   <frame tileid="412" duration="300"/>
+   <frame tileid="418" duration="300"/>
+   <frame tileid="424" duration="300"/>
+   <frame tileid="430" duration="300"/>
+   <frame tileid="436" duration="300"/>
+   <frame tileid="442" duration="300"/>
+   <frame tileid="448" duration="300"/>
+   <frame tileid="454" duration="300"/>
+   <frame tileid="460" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="463">
+  <animation>
+   <frame tileid="463" duration="300"/>
+   <frame tileid="469" duration="300"/>
+   <frame tileid="475" duration="300"/>
+   <frame tileid="481" duration="300"/>
+   <frame tileid="487" duration="300"/>
+   <frame tileid="493" duration="300"/>
+   <frame tileid="499" duration="300"/>
+   <frame tileid="505" duration="300"/>
+   <frame tileid="511" duration="300"/>
+   <frame tileid="517" duration="300"/>
+   <frame tileid="523" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="464">
+  <animation>
+   <frame tileid="464" duration="300"/>
+   <frame tileid="470" duration="300"/>
+   <frame tileid="476" duration="300"/>
+   <frame tileid="482" duration="300"/>
+   <frame tileid="488" duration="300"/>
+   <frame tileid="494" duration="300"/>
+   <frame tileid="500" duration="300"/>
+   <frame tileid="506" duration="300"/>
+   <frame tileid="512" duration="300"/>
+   <frame tileid="518" duration="300"/>
+   <frame tileid="524" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="465">
+  <animation>
+   <frame tileid="465" duration="300"/>
+   <frame tileid="471" duration="300"/>
+   <frame tileid="477" duration="300"/>
+   <frame tileid="483" duration="300"/>
+   <frame tileid="489" duration="300"/>
+   <frame tileid="495" duration="300"/>
+   <frame tileid="501" duration="300"/>
+   <frame tileid="507" duration="300"/>
+   <frame tileid="513" duration="300"/>
+   <frame tileid="519" duration="300"/>
+   <frame tileid="525" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="466">
+  <animation>
+   <frame tileid="466" duration="300"/>
+   <frame tileid="478" duration="300"/>
+   <frame tileid="484" duration="300"/>
+   <frame tileid="490" duration="300"/>
+   <frame tileid="496" duration="300"/>
+   <frame tileid="502" duration="300"/>
+   <frame tileid="508" duration="300"/>
+   <frame tileid="514" duration="300"/>
+   <frame tileid="520" duration="300"/>
+   <frame tileid="526" duration="300"/>
   </animation>
  </tile>
  <wangsets>
